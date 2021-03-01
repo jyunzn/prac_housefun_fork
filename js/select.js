826b275609854b5a8b03ca80f117fbf641cf8a17
jyunzn <jyunzn@gmail.com>
2021-03-01 13:16:30 +0800

    城市tag按鈕完成

--- js/select.js
+++ js/select.js
@@ -28,10 +28,16 @@
     let oPurposes = document.querySelector('.select-area .purposes');
     let oPriceRange = document.querySelector('.select-area .price-range');
 
+    // 最下面的標籤
+    let oTagAreas = document.querySelector('.select-area .selecting-areas');
+    let oTagPurposes = document.querySelector('.select-area .selecting-purposes');
+    let oTagPrice = document.querySelector('.select-area .selecting-price');
+
     
     // 其他
     let oSelectAreaTipBox = oselectArea.querySelector('.select-area-tip-box'); // 小提示框
-    let aPriceInputBox = oPriceRange.querySelectorAll('.price-input-box input'); // 輸入金額的匡
+    let oPriceInputN1 = oPriceRange.querySelector('.price-input-box .n1');
+    let oPriceInputN2 = oPriceRange.querySelector('.price-input-box .n2');
 
 
     oCityBtn.addEventListener('click', handleCityBtn);
@@ -45,7 +51,8 @@
     aPriceRangeOption.forEach(dom => dom.addEventListener('click', handlePriceRangeOptions));
 
     document.addEventListener('click', handleDocumentClick);
-    aPriceInputBox.forEach(dom => dom.addEventListener('blur', handlePriceInputBlur));
+    oPriceInputN1.addEventListener('blur', handlePriceInputBlur);
+    oPriceInputN2.addEventListener('blur', handlePriceInputBlur);
 
     // 最上面四顆按鈕的點擊事件處理
     function handleCityBtn(ev) {
@@ -82,14 +89,10 @@
         ev.cancelBubble = true;
     }
 
-
     function handleDisplayArea(displayDom, hiddenDoms) {
         hiddenDoms.forEach(dom => (dom.style.display = 'none'));
         displayDom.style.display = 'block';
     }
-
-
-
 
     function handleDisableClass() {
         if (selectCount >= 3) {
@@ -106,6 +109,52 @@
         }
     }
 
+    function handleTagClick(ev) {
+        if (this.dataset.city === '0') {
+            let [city, area] = this.innerText.split('-');
+
+            // 刪除指定區域
+            removeArea(city, area);
+
+            // 如果刪除的城市是現在選中的區域城市，那麼要把那個類刪掉
+            if (curSelectCity == city) {
+                Array.from(aAreaOption).find(dom => dom.innerText == area).classList.remove('select');
+            }
+
+            // 處理按鈕本身
+                // 如果當前城市已經沒有選擇的區域，就把按鈕改成城市鈕
+            if (selectAreas[city].length == 0) {
+                this.textContent = city;
+                this.setAttribute('data-city', city);
+            } else {
+                // 如果還有的話，就刪掉按鈕本身
+                this.parentElement.removeChild(this);
+            }
+        } else {
+            let str = this.innerText;
+
+            // 刪除城市數據
+            removeCity(str);
+            
+            // 刪除城市的選擇類
+            Array.from(aCityOption).find(dom => dom.innerText == str).classList.remove('select');
+            
+            // 看看還有沒有選項，要處理小提示
+            handleTips();
+
+            // 刪除按鈕本身
+            this.parentElement.removeChild(this);
+
+            // 回到城市選單，重新選擇城市
+            curSelectCity = '';
+            moveAreaOptions(false);
+        }
+        
+        console.log(selectAreas, selectCount);
+        handleDisableClass();
+        ev.cancelBubble = true;
+    }
+
     let selectAreas = {};
     let curSelectCity = '';
     let selectCount = 0;
@@ -115,8 +164,9 @@
     function handleCityOptions(ev) {
         // 判斷是否是點右上角的刪除 icon
         if (ev.target.dataset.remove) {
-            curSelectCity = this.innerText.trim();
-            removeCity(curSelectCity);
+            let tmp = this.innerText.trim();
+            removeCity(tmp);
+            curSelectCity = '';
             this.classList.remove('select');
         } else {
             let tmp = this.innerText.trim();
@@ -133,7 +183,8 @@
             curSelectCity = tmp;
 
             // 數據處理
-            addCity(curSelectCity);
+            addCityTag();
+            addCity();
             generAreaList(my_city[curSelectCity]);
             moveAreaOptions(true);
             this.classList.add('select');
@@ -146,16 +197,15 @@
         ev.cancelBubble = true;
     }
 
-    function addCity(city) {
-        if (!selectAreas[city]) {
-            selectAreas[city] = []
+    function addCity() {
+        if (!selectAreas[curSelectCity]) {
+            selectAreas[curSelectCity] = []
             selectCount++
         }
     }
     function removeCity(city) {
         len = selectAreas[city].length == 0 ? 1 : selectAreas[city].length;
         delete selectAreas[city] && (selectCount -= len);
-        curSelectCity = '';
     }
     function handleTips() {
         let text = '';
@@ -167,17 +217,37 @@
         oSelectAreaTipBox.textContent = text;
     }
 
+
+    function addCityTag() {
+        if (selectAreas[curSelectCity] === undefined) {
+            oTagAreas.appendChild(genCityTag());
+        }
+    }
+    function genCityTag() {
+        const oBtn = document.createElement('button');
+        oBtn.classList.add('tag');
+        oBtn.setAttribute('data-city', curSelectCity);
+        oBtn.textContent = curSelectCity;
+        oBtn.addEventListener('click', handleTagClick);
+        return oBtn;
+    }
+
+
     // 區選項
     function handleAreaOptions(ev) {
-        // 最多三個，但是如果當前的城市還沒選區的話可以放行
+        
         let seletedArea = this.innerText.trim();
+
+        // 移除按鈕
         if (ev.target.dataset.remove) {
-            removeArea(seletedArea);
+            removeArea(curSelectCity, seletedArea);
             this.classList.remove('select');
         } else {
+            // 最多三個，但是如果當前的城市還沒選區的話可以放行
             if (selectCount >= 3 && selectAreas[curSelectCity].length !== 0) return;
             if (addArea(seletedArea)) {
                 this.classList.add('select');
+                addAreaTag(seletedArea);
             };
         }
         console.log(selectAreas, selectCount);
@@ -198,8 +268,8 @@
         return true;
     }
 
-    function removeArea(area) {
-        let curCityInfos = selectAreas[curSelectCity];
+    function removeArea(city, area) {
+        let curCityInfos = selectAreas[city];
         let index = curCityInfos.findIndex(v => v == area);
         index >= 0 && curCityInfos.splice(index, 1);
 
@@ -208,6 +278,25 @@
             selectCount--
         }
         
+    }
+
+    function addAreaTag(area) {
+        if (selectAreas[curSelectCity].length == 1) {
+            const oTag = oTagAreas.querySelector(`.tag[data-city=${curSelectCity}]`);
+            oTag.textContent = `${curSelectCity}-${area}`;
+            oTag.setAttribute('data-city', 0);
+        } else {
+            oTagAreas.appendChild(genAreaTag(area));
+        }
+        console.log(selectAreas[curSelectCity], area);
+    }
+    function genAreaTag(area) {
+        const oBtn = document.createElement('button');
+        oBtn.classList.add('tag');
+        oBtn.textContent = `${curSelectCity}-${area}`;
+        oBtn.setAttribute('data-city', 0);
+        oBtn.addEventListener('click', handleTagClick);
+        return oBtn;
     }
 
 
@@ -228,10 +317,15 @@
     // 價格區間
     let selectPriceRange = '不限';
     function handlePriceRangeOptions(ev) {
+        console.log(this.dataset.cuz);
         selectPriceRange = this.innerText.trim();
         handlePriceValue(selectPriceRange);
         aPriceRangeOption.forEach(dom => dom.classList.remove('select'));
         this.classList.add('select');
+
+        if (!this.dataset.cuz) {
+            resetCuzPriceRange();
+        }
 
         console.log(selectPriceRange);
         ev.cancelBubble = true;
@@ -250,22 +344,44 @@
     let n1 = null;
     let n2 = null;
     function handlePriceInputBlur(ev) {
-        // let input = Number(this.value);
-        // if (Number.isNaN(input)) {
-        //     this.value = '';
-        //     return;
-        // }
+        let input = Number(this.value);
+            input = input ? input : null; 
 
-        // let n = this.dataset.n;
-        // if (n == 'n1') {
-        //     n1 = input;
-        //     if (n1 > n2) {
-                
-        //     }
-        // } else {
-        //     n2 = input;
-        // }
-        
+        let n = this.dataset.n;
+        if (n == 'n1') {
+            if (input == null) {
+                this.value = '';
+                n1 = null;
+            } else {
+                n1 = input;
+                if (n2 != null && n1 > n2) {
+                    [n1, n2] = [n2, n1];
+                    oPriceInputN1.value = n1;
+                    oPriceInputN2.value = n2;
+                }
+            }
+        } else {
+            if (input == null) {
+                this.value = '';
+                n2 = null;
+            } else {
+                n2 = input;
+                if (n1 != null && n1 > n2) {
+                    [n1, n2] = [n2, n1];
+                    oPriceInputN1.value = n1;
+                    oPriceInputN2.value = n2;
+                }
+            }
+        }
+
+        console.log(n1, n2);
+    }
+
+    function resetCuzPriceRange() {
+        n1 = null;
+        n2 = null;
+        oPriceInputN1.value = '';
+        oPriceInputN2.value = '';
     }
 
 
@@ -318,7 +434,7 @@
             html = areas.map(area => {
                 isSelected = selectAreas[curSelectCity].includes(area);
                 return `
-                    <div class="option ${isSelected ? 'select': ''}">
+                    <div class="option ${isSelected ? 'select': ''}" data-area="${area}">
                         <input type="checkbox" class="input"}>
                         <span>${area}</span>
                         <i class="check-icon"><svg t="1614409506986" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3419" width="200" height="200" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><style type="text/css"></style></defs><path d="M1024 246.4l-128-134.4-512 544-256-273.6-128 136 256 273.6L384 928l128-136z" p-id="3420" fill="#60A060"></path></svg></i>
@@ -329,7 +445,7 @@
         } else {
             // 沒有就直接生成
             html = areas.map(area => `
-                <div class="option">
+                <div class="option" data-area="${area}">
                     <input type="checkbox" class="input"}>
                     <span>${area}</span>
                     <i class="check-icon"><svg t="1614409506986" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3419" width="200" height="200" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><style type="text/css"></style></defs><path d="M1024 246.4l-128-134.4-512 544-256-273.6-128 136 256 273.6L384 928l128-136z" p-id="3420" fill="#60A060"></path></svg></i>
